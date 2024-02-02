eb6f69392206a74da06b9e697bcbdb0a729c8f99
lovenoel <kinglovenoel@gmail.com>
2024-02-02 10:06:16 -0800

    tasks

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -6,7 +6,7 @@
  * @ht: the hash table to add or update the key/value to
  * @key: the key (cannot be an empty string)
  * @value: the value associated with the key (must be duplicated)
- * Return: 1 if successful, 0 otherwise
+ * Return: 1 if it succeeded, 0 otherwise
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
@@ -22,8 +22,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
     while (current != NULL)
     {
         if (strcmp(current->key, key) == 0)
-            return update_node_value(current, value);
-
+        {
+            /* Key already exists, update value and return */
+            free(current->value);
+            current->value = strdup(value);
+            if (current->value == NULL)
+                return (0);
+            return (1);
+        }
         current = current->next;
     }
 
