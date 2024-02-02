ea6b2b5431222aa0c73cd29455974a5486ccafb1
lovenoel <kinglovenoel@gmail.com>
2024-02-03 00:30:34 +0300

    tasks

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,26 +10,52 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
-	hash_node_t *new_node, *current;
+    unsigned long int index;
+    hash_node_t *new_node, *current;
 
-	if (ht == NULL || key == NULL || *key == '\0')
-		return (0);
-	index = key_index((unsigned char *)key, ht->size);
-	current = ht->array[index];
+    if (ht == NULL || key == NULL || *key == '\0')
+        return (0);
 
-	while (current != NULL)
-	{
-		if (strcmp(current->key, key) == 0)
-			return (update_node_value(current, value));
-		current = current->next;
-	}
-	/* Key doesn't exist, create a new node */
-	new_node = create_new_node(key, value);
-	if (new_node == NULL)
-		return (0);
-	/* Add the new node at the beginning of the linked list */
-	new_node->next = ht->array[index];
-	ht->array[index] = new_node;
-	return (1);
+    index = key_index((unsigned char *)key, ht->size);
+    current = ht->array[index];
+
+    while (current != NULL)
+    {
+        if (strcmp(current->key, key) == 0)
+        {
+            /* Key already exists, update value and return */
+            free(current->value);
+            current->value = strdup(value);
+            if (current->value == NULL)
+                return (0);
+            return (1);
+        }
+        current = current->next;
+    }
+
+    /* Key doesn't exist, create a new node */
+    new_node = malloc(sizeof(hash_node_t));
+    if (new_node == NULL)
+        return (0);
+
+    new_node->key = strdup(key);
+    if (new_node->key == NULL)
+    {
+        free(new_node);
+        return (0);
+    }
+
+    new_node->value = strdup(value);
+    if (new_node->value == NULL)
+    {
+        free(new_node->key);
+        free(new_node);
+        return (0);
+    }
+
+    /* Add the new node at the beginning of the linked list */
+    new_node->next = ht->array[index];
+    ht->array[index] = new_node;
+
+    return (1);
 }
