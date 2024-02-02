5624f14e58286a1d431939abdb9e639f0cff220c
lovenoel <kinglovenoel@gmail.com>
2024-02-02 09:58:36 -0800

    tasks

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,52 +10,25 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned long int index;
-    hash_node_t *new_node, *current;
+	if (!ht || !key || !*key)
+		return (0);
 
-    if (ht == NULL || key == NULL || *key == '\0')
-        return (0);
+	unsigned long int index = key_index((unsigned char *)key, ht->size);
+	hash_node_t *current = ht->array[index];
 
-    index = key_index((unsigned char *)key, ht->size);
-    current = ht->array[index];
+	while (current && strcmp(current->key, key) && (current = current->next));
 
-    while (current != NULL)
-    {
-        if (strcmp(current->key, key) == 0)
-        {
-            /* Key already exists, update value and return */
-            free(current->value);
-            current->value = strdup(value);
-            if (current->value == NULL)
-                return (0);
-            return (1);
-        }
-        current = current->next;
-    }
+	if (current)
+		return (update_node_value(current, value));
 
-    /* Key doesn't exist, create a new node */
-    new_node = malloc(sizeof(hash_node_t));
-    if (new_node == NULL)
-        return (0);
-
-    new_node->key = strdup(key);
-    if (new_node->key == NULL)
-    {
-        free(new_node);
-        return (0);
-    }
-
-    new_node->value = strdup(value);
-    if (new_node->value == NULL)
-    {
-        free(new_node->key);
-        free(new_node);
-        return (0);
-    }
-
-    /* Add the new node at the beginning of the linked list */
-    new_node->next = ht->array[index];
-    ht->array[index] = new_node;
-
-    return (1);
+	hash_node_t *new_node = malloc(sizeof(hash_node_t));
+	if (!new_node || !(new_node->key = strdup(key)) ||
+			!(new_node->value = strdup(value)))
+	{
+		free(new_node);
+		return (0);
+	}
+	new_node->next = ht->array[index];
+       	ht->array[index] = new_node;
+	return (1);
 }
