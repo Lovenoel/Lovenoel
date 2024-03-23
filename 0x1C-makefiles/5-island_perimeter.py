9b66baabc68b913dc143750862f8901ad0ee94d2
lovenoel <kinglovenoel@gmail.com>
2024-03-24 00:47:42 +0300

    tasks

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,18 +1,29 @@
 #!/usr/bin/python3
+"""
+Module to calculate the perimeter of the island described in a grid.
+"""
 
 def island_perimeter(grid):
     """
-    Calculate the perimeter of the island described in the grid.
+    Calculates the perimeter of the island described in the grid.
 
     Args:
-        grid (list of list of int): A 2D grid representing the island, where 0 represents water and 1 represents land.
+        grid (List[List[int]]): A list of lists of integers representing the grid.
 
     Returns:
         int: The perimeter of the island.
 
+    Example:
+        grid = [
+            [0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0],
+            [0, 1, 1, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0]
+        ]
+        island_perimeter(grid) returns 12
     """
     perimeter = 0
-
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j] == 1:
@@ -21,5 +32,14 @@ def island_perimeter(grid):
                     perimeter -= 2
                 if j > 0 and grid[i][j - 1] == 1:
                     perimeter -= 2
-
     return perimeter
+
+if __name__ == "__main__":
+    grid = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+    ]
+    print(island_perimeter(grid))
