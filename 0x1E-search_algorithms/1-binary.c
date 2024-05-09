02828618f4e15312f77a2427a61b3f9cbd56dc25
lovenoel <kinglovenoel@gmail.com>
2024-05-08 22:49:23 -0700

    tasks

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -11,17 +11,17 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
+	size_t i, left = 0, right = size - 1, mid;
+
 	if (array == NULL)
 		return (-1);
-
-	size_t left = 0, right = size - 1, mid;
 
 	while (left <= right)
 	{
 		mid = (left + right) / 2;
 
 		printf("Searching in array: ");
-		for (size_t i = left; i <= right; i++)
+		for (i = left; i <= right; i++)
 		{
 			if (i != left)
 				printf(", ");
