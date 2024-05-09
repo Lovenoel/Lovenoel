6fb8e5a641894509d384a491bef296bc5864c14b
lovenoel <kinglovenoel@gmail.com>
2024-05-08 19:27:10 -0700

    tasks

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,36 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+/* Standard Libraries */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/* Data Structure for Singly Linked List */
+typedef struct listint_s
+{
+    int n;
+    size_t index;
+    struct listint_s *next;
+} listint_t;
+
+/* Data Structure for Skip List */
+typedef struct skiplist_s
+{
+    int n;
+    size_t index;
+    struct skiplist_s *next;
+    struct skiplist_s *express;
+} skiplist_t;
+
+/* Function Prototypes */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
+
+#endif /* SEARCH_ALGOS_H */
